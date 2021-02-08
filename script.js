7b60bc2210065a4b2c013e5e0bd83b20d6600926
Mohammad Kamrul Hasan <kamrulhasan59246@gmail.com>
2021-02-08 16:09:08 +0000

    Fixed Label scripts

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Configure Slider
 document.getElementById("slider").oninput = function() {
+  document.getElementById("price").innerHTML = this.value + ".00";
   var value = (this.value-this.min)*100/(this.max-this.min);
   this.style.background = 'linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%) ' + value + '%, hsl(224, 65%, 95%) ' + value + '%, hsl(224, 65%, 95%) 100%)';
-  updatePrice();
+  var amnt = document.getElementById("amnt");
+  if (this.value === "8") {
+    amnt.innerHTML = "10K";
+  }
+  else if (this.value === "12") {
+    amnt.innerHTML = "50K";
+  }
+  else if (this.value === "16") {
+    amnt.innerHTML = "100K";
+  }
+  else if (this.value === "24") {
+    amnt.innerHTML = "500K";
+  }
+  else if (this.value === "36") {
+    amnt.innerHTML = "1M";
+  }
 };
 
 //Configure Slider value
@@ -21,7 +37,7 @@ slider = document.getElementById("slider");
 
 function updatePrice() {
   var amnt =document.getElementById("amnt");
-  if (this.value === "8") {
+  if (this.value === 8) {
     amnt.innerHTML = "10K";
   }
   else if (this.value === 12) {
